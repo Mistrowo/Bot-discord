10bc0d4bdf9eb02ebf52864f7eea8671235f0d6e
Mistrowo <alexslipk3@gmail.com>
2023-05-26 21:08:47 -0400

    boton con nuevos mensajes

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 
 const { Client } = require('discord.js');
 const config = require('./config.json');
-const { Hola } = require('./mensajes/mensajes');
+const { Hola, Link } = require('./mensajes/mensajes');
 
 const cliente = new Client({
   intents: 3276799
@@ -16,5 +16,7 @@ cliente.login(config.token);
 
 cliente.on('messageCreate', async (message) => {
   Hola(message);
+  Link(message);
+ 
   
 });
--- mensajes/mensajes.js
+++ mensajes/mensajes.js
@@ -1,13 +1,31 @@
-
+const { MessageActionRow, MessageButton } = require('discord.js');
 
 function Hola(message) {
   if (message.content.toLowerCase() === 'hola') {
     message.channel.send('wena klo');
+
+  } else if (message.content.toLowerCase() === 'fome') {
+    message.channel.send('Saco Wea Culiao');
+  } else if (message.content.toLowerCase() === 'gracias') {
+    message.channel.send('De nada waxito ');
+  } 
+}
+
+function Link(message) {
+  if (message.content.toLowerCase() === 'boton') {
+    const row = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setStyle('LINK')
+        .setLabel('Hola')
+        .setURL('https://github.com/Mistrowo') // Reemplaza con tu enlace deseado
+        .setEmoji('😈')
+    );
+
+    message.channel.send({  components: [row] });
   }
 }
 
-
 module.exports = {
-  Hola
-  
+  Hola,
+  Link
 };
